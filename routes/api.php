6c280fe93ffb1f6d3a92c76656b9bdd9adc030c7
youcef benabderrezak <youcef12091997@gmail.com>
2021-05-19 11:22:56 +0100

    config routes web&api

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,17 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\API\BookController;
+use App\Http\Controllers\API\UserController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::post('login', [UserController::class, 'login']);
+Route::post('register', [UserController::class, 'register']);
+Route::post('logout', [UserController::class, 'logout'])->middleware('auth:sanctum');
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['prefix' => 'books', 'middleware' => 'auth:sanctum'], function () {
+    Route::get('/', [BookController::class, 'index']);
+    Route::post('add', [BookController::class, 'add']);
+    Route::get('edit/{id}', [BookController::class, 'edit']);
+    Route::post('update/{id}', [BookController::class, 'update']);
+    Route::delete('delete/{id}', [BookController::class, 'delete']);
 });
